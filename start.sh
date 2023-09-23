696fd9fca0f22ebb85f152836abc8d4f17b1409f
htmlboss123 <91206261+htmlboss123@users.noreply.github.com>
2023-09-22 21:35:53 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
   git clone https://github.com/htmlboss123/AutoFilterAdvance /AutoFilterAdvance
-elsej
+else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterAdvance
 fi
